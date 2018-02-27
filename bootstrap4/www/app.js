f3e0ea76f0ad61550cd5c1a843920faf26eb9cf5
Brian Jones <brian.jones@geotoolkit.net>
2018-02-27 10:58:44 -0600

    Use 'bundle' instead of 'popper' (Working, but will create console error)

--- bootstrap4/www/app.js
+++ bootstrap4/www/app.js
@@ -2,11 +2,12 @@
 requirejs.config({
     paths: {
         'jquery': 'https://code.jquery.com/jquery-3.2.1.slim.min',
-        // 'bootstrap': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min'
+        'bundle': 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min',
+        'bootstrap': 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min'
     },
-    // shim: {
-        // 'bootstrap': ['jquery']
-    // }
+    shim: {
+        'bootstrap': ['jquery', 'bundle']
+    }
 });
 
 requirejs(['app/main']);
